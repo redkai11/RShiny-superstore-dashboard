439b2976856a8d7f6188d1d12ed0d89a256d9359
redkai11 <lakonpark@gmail.com>
2022-06-16 00:12:04 -0700

    finishehd general layout

---
+++ server.R
@@ -0,0 +1,156 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(leaflet)
+library(DBI)
+library(dplyr)
+library(lubridate)
+library(plotly)
+
+
+# SQL Server Connection ---------------------------------------------------------
+
+azure_credentials <- read.table("KEY-FILE.txt", sep = ",", header = FALSE)
+uid <- azure_credentials[1,1]
+pwd <- azure_credentials[1,2]
+con <- dbConnect(odbc::odbc(), .connection_string = "Driver={SQL Server};", 
+                 server = "redkai11-superstore.database.windows.net", database = "superstore", 
+                 uid = uid, pwd = pwd, timeout = 10)
+to_date <- '2020-05-01'
+
+shinyServer(function(input, output) {
+    
+  computing <- reactiveVal(FALSE)
+  trigger <- debounce(computing, 8) # Enough delay for Shiny to render the Spinner first
+  observeEvent(input$search, computing(TRUE))
+  
+  observeEvent(trigger(),{
+    if(trigger()){
+      Sys.sleep(3)
+      computing(FALSE)
+    }
+  })
+  
+  output$spinner <- renderReact({
+    if (computing()) Spinner(size = 3, label = "Loading, please wait...")
+  })
+  
+  
+  filtered_deals <- eventReactive(input$search, {
+    req(input$fromDate)
+    query <- paste("SELECT * FROM [dbo].[Orders] WHERE [Order Date] >= '", input$fromDate, "' AND [Order Date] <= '", input$toDate,"'", sep = "")
+    filtered_deals <- dbGetQuery(con, query)
+  })
+  
+  output$analysis <- renderUI({
+    req(input$filtered_deals)
+    items_list <- if(nrow(filtered_deals()) > 0){
+      DetailsList(items = filtered_deals())
+    } else {
+      p("No matching transactions.")
+    }
+    
+    Stack(
+      tokens = list(childrenGap = 5),
+      Text(variant = "large", "Sales deals details", block = TRUE),
+      div(style="max-height: 500px; overflow: auto", items_list)
+    )
+  })
+  
+  
+  overview_orders_filtered <- reactive({
+    req(input$overview_filter_date)
+    from_date <- as.Date(to_date) - as.numeric(input$overview_filter_date)
+    query <- paste("SELECT * FROM [dbo].[Orders] WHERE [Order Date] >= '", from_date, "' AND [Order Date] <= '", to_date,"'", sep = "")
+    overview_orders_filtered <- dbGetQuery(con, query)
+  })
+  
+  overview_returns_filtered <- reactive({
+    req(input$overview_filter_date)
+    from_date <- as.Date(to_date) - as.numeric(input$overview_filter_date)
+    query <- paste("SELECT * FROM [dbo].[Orders], [dbo].[Returns]",
+                   "WHERE [dbo].[Orders].[Order ID] = [dbo].[Returns].[Order ID] AND [Order Date] >= '",
+                   from_date, "' AND [Order Date] <= '", to_date,"'",
+                   " AND [dbo].[Returns].[Returned] = 'yes'", sep = "")
+    print(query)
+    overview_returns_filtered <- dbGetQuery(con, query)
+  })
+  
+  output$customers_in_date_range <- renderText({
+    req(overview_orders_filtered)
+    length(unique(overview_orders_filtered()$`Customer ID`))
+  })
+  
+  output$revenue_in_date_range <- renderText({
+    req(overview_orders_filtered)
+    paste("$ ", round(sum(overview_orders_filtered()$Sales),2), sep ="")
+  })
+  
+  output$orders_in_date_range <- renderText({
+    req(overview_orders_filtered)
+    nrow(overview_orders_filtered())
+  })
+  
+  output$returns_in_date_range <- renderText({
+    req(overview_returns_filtered)
+    nrow(overview_returns_filtered())
+  })
+  
+  overview_returns_filtered_previous_date_range <- reactive({
+    req(input$overview_filter_date)
+    from_date <- as.Date(to_date) - 2 * 30
+    to_date <- as.Date(to_date) - 30
+    query <- paste("SELECT * FROM [dbo].[Orders], [dbo].[Returns]",
+                   "WHERE [dbo].[Orders].[Order ID] = [dbo].[Returns].[Order ID] AND [Order Date] >= '",
+                   from_date, "' AND [Order Date] <= '", to_date,"'",
+                   " AND [dbo].[Returns].[Returned] = 'yes'", sep = "")
+    overview_returns_filtered_previous_date_range <- dbGetQuery(con, query)
+  })
+  
+  output$returns_previous_date_range <- renderText({
+    req(overview_returns_filtered_previous_date_range)
+    req(overview_returns_filtered)
+    previous <- nrow(overview_returns_filtered_previous_date_range())
+    current <- nrow(overview_returns_filtered())
+    result <- round((current - previous)/previous * 100)
+  })
+  
+  report_filtered <- reactive({
+    req(input$report_type)
+    req(input$report_filter_date)
+    from_date <- as.Date(to_date) - as.numeric(input$report_filter_date)
+    query <- paste("SELECT [Order Date], sum([Sales])",
+                   " FROM [dbo].[Orders] WHERE [Order Date] >= '", from_date, "' AND [Order Date] <= '", to_date,"'",
+                   " GROUP BY [Order Date]", sep = "")
+    print(query)
+    report_filtered <- dbGetQuery(con, query)
+    report_filtered$'Order Date' <- lubridate::ymd(report_filtered$'Order Date')
+    colnames(report_filtered) <- c("Date", input$report_type)
+    return(report_filtered)
+  })
+  
+  output$report <- renderPlotly({
+    req(report_filtered)
+    req(input$report_type)
+    fig <- plot_ly() %>%
+      add_trace(data = report_filtered(), type = 'scatter', mode = 'lines', 
+                fill = 'tozeroy', x = ~Date, y = ~report_filtered()[,2], name = 'GOOG',
+                fillcolor = 'rgba(168, 216, 234, 0.5)') %>%
+      layout(showlegend = F, yaxis = list(title = input$report_type,
+                                          zerolinecolor = '#ffff',
+                                          zerolinewidth = 2,
+                                          gridcolor = 'ffff'),
+             xaxis = list(zerolinecolor = '#ffff',
+                          zerolinewidth = 2,
+                          gridcolor = 'ffff'),
+             plot_bgcolor='#ffffff')
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,314 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shiny.fluent)
+library(shiny.react)
+library(imola)
+library(stringr)
+library(dplyr)
+library(readr)
+library(leaflet)
+library(glue)
+library(purrr)
+library(plotly)
+
+# Global CSS Variables ---------------------------------------------------------
+
+icon_style <- "width: 50px; height: 50px; border-radius: 50%; background-color: #b3dbf2; color : #0078d4;"
+
+## Header Commander Bar
+
+header_commandbar_list <- list(
+  list(
+    key = 'download',
+    text = "Download data",
+    iconProps = list(iconName = "Download")
+  )
+)
+
+
+# Layout ---------------------------------------------------------
+
+makeCard <- function(title, content, size = 12, style = "") {
+  div(
+    class = glue("card ms-depth-8 ms-sm{size} ms-xl{size}"),
+    style = style,
+    Stack(
+      tokens = list(childrenGap = 5),
+      Text(variant = "large", title, block = TRUE),
+      content
+    )
+  )
+}
+
+makePage <- function (title, subtitle, contents) {
+  tagList(div(
+    class = "page-title",
+    span(title, class = "ms-fontSize-32 ms-fontWeight-semibold", style =
+           "color: #323130;"),
+    span(subtitle, class = "ms-fontSize-14 ms-fontWeight-regular", style =
+           "color: #605E5C; margin: 14px;")
+  ),
+  contents)
+}
+
+
+# UI components ---------------------------------------------------------
+app_header <- flexPanel(
+  id = "header",
+  align_items = "center",
+  flex = c(0, 1, 0),
+  img(src = "superstore-logo.png", style = "width: 150px"),
+  div(
+    Text(variant = "xLarge", "| Dashboard", style="color: gray;"), 
+    style = "margin-bottom: 10px;"),
+  CommandBar(items = header_commandbar_list),
+  style = "box-shadow: 0 0 10px #000;"
+)
+
+navigation <- Nav(
+  groups = list(
+    list(links = list(
+      list(name = 'Overview', url = '#!/', key = 'home', icon = 'Home'),
+      list(name = 'Analysis', url = '#!/other', key = 'analysis', icon = 'AnalyticsReport'),
+      list(name = 'shiny.fluent', url = '', key = 'repo', icon = 'GitGraph'),
+      list(name = 'shiny.react', url = '', key = 'shinyreact', icon = 'GitGraph'),
+      list(name = 'Appsilon', url = '', key = 'appsilon', icon = 'WebAppBuilderFragment')
+    ))
+  ),
+  initialSelectedKey = 'home',
+  styles = list(
+    root = list(
+      height = '100%',
+      boxSizing = 'border-box',
+      overflowY = 'auto'
+    )
+  )
+)
+
+
+# KPI Content ---------------------------------------------------------
+
+
+customers_kpi <- Stack(
+  horizontal = TRUE,
+  tokens = list(childrenGap = 10),
+  style = "display: flex; align-items: center;",
+  div(
+    style=icon_style,
+    FontIcon(iconName = "AlignVerticalBottom", style = "font-size: 1.5em; text-align: center; display: block; margin: 10%;")
+  ),
+  div(
+    Stack(
+      Text("Total Customer"),
+      Stack(
+        horizontal = TRUE,
+        style= "margin-top: 16px;",
+        div(
+          class = "kpi-value",
+          style = "color: black; font-size: 2.5rem;",
+          textOutput("customers_in_date_range")
+        ),
+        div(
+          class = "net-change",
+          h3("smt")
+          #textOutput("customers_previous_date_range")
+        )
+      )
+    )
+  )
+)
+
+revenue_kpi <- Stack(
+  horizontal = TRUE,
+  tokens = list(childrenGap = 10),
+  style = "display: flex; align-items: center;",
+  div(
+    style=icon_style,
+    FontIcon(iconName = "AlignVerticalBottom", style = "font-size: 1.5em; text-align: center; display: block; margin: 10%;")
+  ),
+  div(
+    Stack(
+      Text("Total Revenue"),
+      Stack(
+        horizontal = TRUE,
+        style= "margin-top: 16px;",
+        div(
+          class = "kpi-value",
+          style = "color: black; font-size: 2.5rem;",
+          textOutput("revenue_in_date_range")
+        ),
+        div(
+          class = "net-change",
+          Text("+ 10%")
+        )
+      )
+    )
+  )
+)
+
+orders_kpi <- Stack(
+  horizontal = TRUE,
+  tokens = list(childrenGap = 10),
+  style = "display: flex; align-items: center;",
+  div(
+    style=icon_style,
+    FontIcon(iconName = "AlignVerticalBottom", style = "font-size: 1.5em; text-align: center; display: block; margin: 10%;")
+  ),
+  div(
+    Stack(
+      Text("Total Orders"),
+      Stack(
+        horizontal = TRUE,
+        style= "margin-top: 16px;",
+        div(
+          class = "kpi-value",
+          style = "color: black; font-size: 2.5rem;",
+          textOutput("orders_in_date_range")
+        ),
+        div(
+          class = "net-change",
+          Text("+ 10%")
+        )
+      )
+    )
+  )
+)
+
+returns_kpi <- Stack(
+  horizontal = TRUE,
+  tokens = list(childrenGap = 10),
+  style = "display: flex; align-items: center;",
+  div(
+    style=icon_style,
+    FontIcon(iconName = "AlignVerticalBottom", style = "font-size: 1.5em; text-align: center; display: block; margin: 10%;")
+  ),
+  div(
+    Stack(
+      Text("Total Returns"),
+      Stack(
+        horizontal = TRUE,
+        style= "margin-top: 16px;",
+        div(
+          class = "kpi-value",
+          style = "color: black; font-size: 2.5rem;",
+          textOutput("returns_in_date_range")
+        ),
+        div(
+          class = "net-change",
+          textOutput("returns_previous_date_range")
+        )
+      )
+    )
+  )
+)
+
+# Overview Content ---------------------------------------------------------
+
+
+filters <- Stack(
+  tokens = list(childrenGap = 10),
+  Stack(
+    horizontal = TRUE,
+    tokens = list(childrenGap = 10),
+    DatePicker.shinyInput("fromDate", value = as.Date("2016/1/1"), label = "From date"),
+    DatePicker.shinyInput("toDate", value = Sys.Date(), label = "To date"),
+    PrimaryButton.shinyInput("search", text = "Search", style = "width: 150px; margin-top:30px")
+  )
+)
+
+options <- list(
+  list(key = "30", text = "Last 30 Days"),
+  list(key = "7", text = "Last 7 Days"),
+  list(key = "365", text = "Last 365 Days")
+)
+
+kpi <- Stack(
+  horizontal = FALSE,
+  Stack(
+    horizontal = TRUE,
+    class = "kpi-header-wrapper",
+    makeCard("", h3("Overview"), size = 2),
+    makeCard("", Dropdown.shinyInput("overview_filter_date", value = "30", options = options), size = 2, style = "margin-left : auto;")
+  ),
+  Stack(
+    horizontal = TRUE,
+    class = "kpi-content-wrapper",
+    tokens = list(childrenGap = 5),
+    makeCard("", customers_kpi, size = 3),
+    makeCard("", revenue_kpi, size = 3),
+    makeCard("", orders_kpi, size = 3),
+    makeCard("", returns_kpi, size = 3)
+  )
+)
+
+
+report_options <- list(
+  list(key = "Sales", text = "Sales"),
+  list(key = "Profit", text = "Profit")
+)
+
+
+report <- Stack(
+    horizontal = FALSE,
+    class = "report-wrapper",
+    tokens = list(childrenGap = 5),
+    Stack(
+      horizontal = TRUE,
+      makeCard("", h3("Report"), size = 2),
+      makeCard("", Dropdown.shinyInput("report_type", value = "Sales", options = report_options), size = 2, style = "margin-left : 30px;"),
+      makeCard("", Dropdown.shinyInput("report_filter_date", value = "30", options = options), size = 2, style = "margin-left : auto;")
+    ),
+    Stack(
+      makeCard("", plotlyOutput("report"), size = 12)
+    )
+
+  )
+  
+                    
+app_content <- div(
+      makeCard("", kpi, size = 12, style = "background-color: transparent; margin-bottom: 28px"),
+      # Stack(
+      #   horizontal = FALSE,
+      #   tokens = list(childrenGap = 5),
+      #   makeCard("", filters, size = 12, style = "max-height: 600px;"),
+      #   reactOutput("spinner"),
+      #   uiOutput("analysis")
+      # )
+      makeCard("", report, size = 8, style = "background-color: transparent; margin-bottom: 28px")
+    )
+
+app_footer <- flexPanel(
+  id = "footer",
+  justify_content = 'space-between',
+  gap = "20px",
+  Text(variant = "medium", "Built with R.fluent and R.react", block=TRUE),
+  Text(variant = "medium", nowrap = FALSE, "Data source: Data.World")
+)
+
+# Define UI for application that draws a histogram
+shinyUI(
+  gridPage(
+    tags$head(includeCSS("www/styles.css")),
+    template = "grail-left-sidebar",
+    gap = "10px",
+    rows = list(
+      default = "70px 1fr 30px"
+    ),
+    columns = list(
+      default = "minmax(240px, 12%) auto auto"
+    ),
+    header = app_header,
+    sidebar = navigation,
+    content = app_content,
+    footer = app_footer
+  )
+)
+
